822c9966674db92dca5092e004132fa38d043a85
José Froes <josepaulo.froes@gmail.com>
2026-01-21 19:43:57 -0300

    first commit

---
+++ src/test/java/com/josefroes/dscommerce2/Dscommerce2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josefroes.dscommerce2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dscommerce2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
