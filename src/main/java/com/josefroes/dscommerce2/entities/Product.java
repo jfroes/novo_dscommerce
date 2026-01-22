f5f1f69f322206d55b0f0322573be200a5ffa06a
José Froes <josepaulo.froes@gmail.com>
2026-01-22 16:47:56 -0300

    MUITOS PARA MUITOS C/ CLASSE AUXILIAR PARA CHAVE COMPOSTA

--- src/main/java/com/josefroes/dscommerce2/entities/Product.java
+++ src/main/java/com/josefroes/dscommerce2/entities/Product.java
@@ -4,6 +4,7 @@ package com.josefroes.dscommerce2.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -27,6 +28,9 @@ public class Product {
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -81,5 +85,13 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders(){
+        return items.stream().map(x -> x.getOrder()).toList();
     }
 }
