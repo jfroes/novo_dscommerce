9efaec1f8e69900476fae3174a4ba1675d92f4c7
José Froes <josepaulo.froes@gmail.com>
2026-01-27 17:52:50 -0300

    Add ProductController

---
+++ src/main/java/com/josefroes/dscommerce2/controllers/ProductController.java
@@ -0,0 +1,26 @@
+package com.josefroes.dscommerce2.controllers;
+
+import com.josefroes.dscommerce2.entities.Product;
+import com.josefroes.dscommerce2.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @GetMapping
+    public String test() {
+        Optional<Product> result = repository.findById(54L);
+        Product product = result.get();
+
+        return product.getName();
+    }
+}
